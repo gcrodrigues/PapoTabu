1b186467696ff32efb6c8dc56989857f83cda53e
fefetey <fefetroncoso@live.com>
2019-10-15 20:54:28 -0300

    Adição do CSS e JS + link no html

---
+++ script/scroll.js
@@ -0,0 +1,18 @@
+$("#opcao1").click(function() {
+    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 50}, 1000);
+})
+$("#opcao2").click(function() {
+    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000); 
+})
+$("#opcao3").click(function() {
+    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
+})
+$("#opcao4").click(function() {
+    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
+})
+$("#opcao5").click(function() {
+    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
+})
+$("#opcao6").click(function() {
+    $("#Scroll").animate({scrollTop: $("##id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
+})
