b149a08fc274b2615ec5b0b1ccd326ddd9d90103
Gustavo <gcrodrigues413@gmail.com>
2019-11-03 05:10:19 -0300

    Script scroll de links internos

--- script/scroll.js
+++ script/scroll.js
@@ -1,18 +1,10 @@
-$("#opcao1").click(function() {
+/*$("#opcao1").click(function() {
     $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 50}, 1000);
-})
-$("#opcao2").click(function() {
-    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000); 
-})
-$("#opcao3").click(function() {
-    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
-})
-$("#opcao4").click(function() {
-    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
-})
-$("#opcao5").click(function() {
-    $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
-})
-$("#opcao6").click(function() {
-    $("#Scroll").animate({scrollTop: $("##id da classe").offset().top - 65 + $("#Scroll").scrollTop()}, 1000);
-})
+})*/
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2500);
+    });
+});
+
