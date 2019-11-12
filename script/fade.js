fb4681cf8b517d111bd6c20cdb8f12eec8af8531
Gustavo <gcrodrigues413@gmail.com>
2019-11-12 04:01:25 -0300

    Mudança no script de fade do header

--- script/fade.js
+++ script/fade.js
@@ -1,3 +1,16 @@
-$(window).scroll(function(){
-    $(".fadeout").css("opacity", 1 - $(window).scrollTop() / 450);
-});
+var nystories = document.querySelector(".main").offsetTop;
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+        var opac = (window.pageYOffset / nystories);
+        var titulo = document.querySelector(".elementos-flex");
+        document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('images/bgcapitulo.png') no-repeat";
+        
+        if($(window).scrollTop()>80){
+            titulo.style.color = "black";
+            titulo.style.transition = "color 1s"
+        }else{
+            titulo.style.color = "white";
+            titulo.style.transition = "color 1s"
+        }
+    }
+}
