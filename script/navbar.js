d9699fc04ea5922c1ff8e48f9bb7b9fa428aa346
Gustavo <gcrodrigues413@gmail.com>
2019-11-18 13:47:18 -0300

    Correções na barra de navegação

--- script/navbar.js
+++ script/navbar.js
@@ -7,3 +7,22 @@ $(function() {
         }
     });
 });
+
+
+var toggle = document.querySelector(".toggle");
+var controle = 0;
+toggle.addEventListener("click", function(){
+    var nav = document.querySelector(".flex-nav");
+    if(controle == 0){
+        var insta = document.querySelector("#insta");
+        insta.style.order= 1;
+        nav.classList.add("column-direction");
+        
+        controle++
+    }else{
+        nav.classList.remove("column-direction");
+        controle--
+    }
+    console.log(controle);
+    
+});
