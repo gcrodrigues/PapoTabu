ee67d6f6f2ac04836a50b4160dc1806ccd3c2720
Gustavo <gcrodrigues413@gmail.com>
2019-11-26 19:26:44 -0300

    Arquivos inuteis apagados e inserção de imagens nos paragrafos

--- script/scroll.js
+++ script/scroll.js
@@ -1,6 +1,7 @@
 /*$("#opcao1").click(function() {
     $("#Scroll").animate({scrollTop: $("#id da classe").offset().top - 50}, 1000);
 })*/
+
 jQuery(document).ready(function($) {
     $(".scroll").click(function(event){
         event.preventDefault();
